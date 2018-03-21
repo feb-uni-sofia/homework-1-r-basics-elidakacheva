bd40f6d1063d14f480529c9fb6b1bc40b49900af
elida kacheva <ekacheva7@gmail.com>
2018-03-21 22:03:42 +0200

    a

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,29 @@
+# Problem 1
 
+# a)
+x <- c(4, 1, 1, 4)
+
+# b)
+y <- c(1, 4)
+
+# c)
+x - y
+# since the lenght of vector y is smaller than this of x, it enlarges the shorter one by repeating the values; thus it substracts once again the elements of y from the 3rd and the 4th elements of x 
+# in order for this to happen the number of elements in the longer one should be divisible by the number of elements in the shorter one
+
+# d)
+s <- c(x, y)
+
+# e)
+rep(s, 10)
+length(rep(s, 10))
+
+# f)
+rep(s, each = 3)
+
+# g)
+seq(7, 21, by = 1)
+7:21
+
+# h)
+length(seq(7, 21, by = 1))
--- Problem_2.R
+++ Problem_2.R
@@ -1,1 +1,40 @@
+# Problem 2..
 
+# a)
+xmin <- c(23.0, 20.5, 28.2, 20.3, 22.4, 17.2, 18.2)
+xmax <- c(25.0, 22.8, 31.2, 27.3, 28.4, 20.2, 24.1)
+
+# b)
+xmax - xmin
+
+# c)
+avgxmin <- mean(xmin)
+avgxmax <- mean(xmax)
+
+# d)
+xmin[xmin < avgxmin]
+
+# e)
+xmin[xmin > avgxmin]
+
+# f)
+dates <- c('03Mon18', '04Tue18', '05Wed18', '06Thu18', '07Fri18', '08Sat18', '09Sun18')
+names(xmin) <- dates
+xmin
+names(xmax) <- dates
+xmax
+
+# g)
+temperatures <- data.frame(xmin = xmin, xmax = xmax)
+
+# h)
+xminFahrenheit <- 9/5 * xmin + 32
+temperatures <- within(temperatures, {xminFahrenheit <- 9/5 * xmin + 32})
+
+# i)
+xmaxFahrenheit <- 9/5 * xmax + 32
+temperaturesFahrenheit <- data.frame(xminFahrenheit = xminFahrenheit, xmaxFahrenheit = xmaxFahrenheit)
+
+# j)
+iMonFri <- data.frame(xminFahrenheit = xminFahrenheit[1:5], xmaxFahrenheit = xmaxFahrenheit[1:5])
+iiMonFri <- data.frame(xminFahrenheit = xminFahrenheit[-(6:7)], xmaxFahrenheit = xmaxFahrenheit[-(6:7)])
